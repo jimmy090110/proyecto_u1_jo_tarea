58f5b1e2d147d762e2397c88f20d328c1c447e41
Jimmy Ortega <fabricioortega2011@gmail.com>
2022-11-24 21:58:11 -0500

    Tarea Herencia

---
+++ src/main/java/com/example/demo/herencia/ProfesorH.java
@@ -0,0 +1,30 @@
+package com.example.demo.herencia;
+
+public class ProfesorH {
+	private String nombre;
+	private String cedula;
+	
+	
+	
+	@Override
+	public String toString() {
+		return "Profesor [nombre=" + nombre + ", cedula=" + cedula + ", getNombre()=" + getNombre() + ", getCedula()="
+				+ getCedula() + ", getClass()=" + getClass() + ", hashCode()=" + hashCode() + ", toString()="
+				+ super.toString() + "]";
+	}
+	public String getNombre() {
+		return nombre;
+	}
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	public String getCedula() {
+		return cedula;
+	}
+	public void setCedula(String cedula) {
+		this.cedula = cedula;
+	}
+
+
+
+}
--- src/main/java/com/example/demo/tradicional/Main.java
+++ src/main/java/com/example/demo/tradicional/Main.java
@@ -6,6 +6,7 @@ public class Main {
 	public static void main(String[] args) {
 		Examen examen = new Examen();
 		examen.agendar("12345", LocalDateTime.of(2022, 12,1,8,30), "Ingeniero Pablo", "000000000", "Estudiante Victor", "100011111", "RE");
+		
 	}
 
 }
